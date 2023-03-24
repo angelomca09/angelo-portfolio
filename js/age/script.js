e555f188f230540728db07ef40ebebfdae14e0fb
Angelo Munhoz <angelomca09@gmail.com>
2023-03-24 19:38:46 -0300

    Changing Age via Script

---
+++ js/age/script.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  var year = 1999;
+  var month = 6; //July = 7 - 1
+  var day = 10;
+  var today = new Date();
+  console.log("Month: ", today.getMonth());
+  var age = today.getFullYear() - year;
+  if (
+    today.getMonth() < month ||
+    (today.getMonth() == month && today.getDate() < day)
+  ) {
+    age--;
+  }
+  document.querySelector("#age").textContent = age;
+};
