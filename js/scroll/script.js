b993961b65e155b57601db38a941ad1e63f199a2
Angelo Munhoz <angelomca09@gmail.com>
2023-03-25 23:39:54 -0300

    Scroll Script

---
+++ js/scroll/script.js
@@ -0,0 +1,20 @@
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(`header nav a[href*=${id}]`)
+          .classList.add("active");
+      });
+    }
+  });
+});
