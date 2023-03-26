35fd9c1343d1f894881a0a17ce4c2cd53f9c12eb
Angelo Munhoz <angelomca09@gmail.com>
2023-03-26 01:53:50 -0300

    Responsiveness

--- js/scroll/script.js
+++ js/scroll/script.js
@@ -17,4 +17,9 @@ window.addEventListener("scroll", () => {
       });
     }
   });
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 });
